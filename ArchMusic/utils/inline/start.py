d8e005a90991b25d68941c6e7c02fb02c96d219f
Pikachu ツ <91775058+pikaws@users.noreply.github.com>
2023-12-13 21:57:18 +0300

    Update start.py

--- ArchMusic/utils/inline/start.py
+++ ArchMusic/utils/inline/start.py
@@ -123,9 +123,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     InlineKeyboardButton(
                         text=_["S_B_7"], user_id=OWNER
                     ),
-                    InlineKeyboardButton(
-                        text=_["S_B_9"], url=f"https://t.me/Sirinehanimm"
-                    ),
                 ]
             )
     return buttons
